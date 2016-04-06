ca559709922583c4df49b84f1f09c87fc4189e05
Nir Galon <nirgn975@gmail.com>
2016-04-06 14:22:33 +0300

    Run the solr java script in the background.
    Run the solr java script in the background.

---
+++ README.md
@@ -0,0 +1,5 @@
+# solr-script
+Write something about your project, roy.
+
+## Options
+1. `-b | --background` - Add the flag to activate solr in the background. Example: `./solr.sh -b`.
--- solr.sh
+++ solr.sh
@@ -2,7 +2,7 @@
 
 # Download apache solr.
 if [ ! -f solr-4.7.2.zip ]; then
-  wget http://archive.apache.org/dist/lucene/solr/4.7.2/solr-4.7.2.zip 
+  wget http://archive.apache.org/dist/lucene/solr/4.7.2/solr-4.7.2.zip
 fi;
 
 # Exctracting apache solr from the zip file.
@@ -22,5 +22,25 @@ cd -
 
 # Start the server.
 cd solr/example/
+
+# Go through the flags.
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo " "
+      echo "solr-script by roy segall"
+      echo " "
+      echo "options:"
+      echo "-b, --background=TRUE/FALSE     specify if solr will execute in the background or not"
+      exit 0
+      ;;
+    -b|--background)
+      shift
+        java -jar start.jar > /dev/null 2>&1 < /dev/null &
+        exit 0
+      shift
+      ;;
+    esac
+done
 
 java -jar start.jar
